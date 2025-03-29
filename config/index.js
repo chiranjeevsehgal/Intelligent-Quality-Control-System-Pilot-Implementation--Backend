1b7ebf5c2dfc86ab3fd3fa3955e1e2c4c3e0b86a
Chiranjeev Sehgal <72375354+chiranjeevsehgal@users.noreply.github.com>
2025-03-29 10:21:41 +0530

    Port issue fixed

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT || 5000,
+    PORT: 5000,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     GEMINI_MODEL: process.env.GEMINI_MODEL || 'gemini-1.5-flash-001',
 };
