67e46df73493428c568b8dd6833a587d672b8b43
Chiranjeev Sehgal <72375354+chiranjeevsehgal@users.noreply.github.com>
2025-03-29 10:53:49 +0530

    Port conflict Fix

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const http = require("http");
 const WebSocket = require("ws");
 const app = require('./app');
 const config = require('./config');
 const websocketController = require('./controllers/websocket.controller');
 
-const server = http.createServer(app);
+const express = require("express");
+
+const server = app.listen(config.PORT, () => {
+  console.log(`🚀 Server is running on port ${config.PORT}`);
+});
+
 const wss = new WebSocket.Server({ server });
 
 app.set("wss", wss);
 
 wss.on("connection", websocketController.handleWebSocketConnection(wss));
-
-server.listen(config.PORT, () => {
-  console.log(`🚀 Server is running on port ${config.PORT}`);
-});
